b5c184b0cff3a325da63e1805d6697d725b383ac
Jessica Alder <jessalder1989@gmail.com>
2025-10-19 17:24:56 -0700

    Implemented Hitbox Hurtbox detection

---
+++ utility_nodes/hurtbox/hurt_box.gd
@@ -0,0 +1,18 @@
+class_name HurtBox extends Area2D
+
+@export var damage : int = 1
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	area_entered.connect(_area_entered)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _area_entered(area : Area2D) -> void:
+	if area is HitBox:
+		area.take_damage(damage)
